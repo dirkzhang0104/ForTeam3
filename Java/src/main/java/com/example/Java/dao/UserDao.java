a90c8f50cc2947e0d970a4b7127f785b1f37398e
miguel garcia <sinxash8@gmail.com>
2021-09-16 14:25:30 -0400

    trying to add src file

---
+++ Java/src/main/java/com/example/Java/dao/UserDao.java
@@ -0,0 +1,12 @@
+package com.example.Java.dao;
+
+import com.example.Java.model.Questions;
+import com.example.Java.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+    List<User> retrieveAllUsers();
+    User getScoreByName(String name);
+    void addUser(User user);
+}
---
+++ Java/src/main/java/com/example/Java/model/Questions.java
@@ -0,0 +1,42 @@
+package com.example.Java.model;
+
+import java.util.List;
+
+public class Questions {
+    private int questionId;
+    private String question;
+    private List<String> answersList;
+    private int correctAnswer;
+
+    public int getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(int questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public List<String> getAnswersList() {
+        return answersList;
+    }
+
+    public void setAnswersList(List<String> answersList) {
+        this.answersList = answersList;
+    }
+
+    public int getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(int correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+}
