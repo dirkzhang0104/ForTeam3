c0987fefc0cbae721e212361b1155e8c151b48ca
astanley74 <stanley.377@buckeyemail.osu.edu>
2021-09-16 17:10:32 -0400

    add dummy questions to each square

--- team-3-frontend/src/components/Board/Board.js
+++ team-3-frontend/src/components/Board/Board.js
@@ -3,7 +3,7 @@ import Square from '../Square/Square.js'
 import Avatar from '../Avatar/Avatar.js'
 import { moveAvatar } from '../Game/Game.js'
 import { useState } from 'react'
-import { cloneDeep } from 'lodash/cloneDeep'
+// import { cloneDeep } from 'lodash/cloneDeep'
 
 // function renderSquare(i, [avatarX, avatarY]) {
 
@@ -56,19 +56,37 @@ export default function Board({ avatarPosition }) {
 
   function renderSquare(i, [avatarX, avatarY]) {
 
-    const x = i % 4
-    const y = Math.floor(i / 4)
+    const x = i % 5
+    const y = Math.floor(i / 5)
   
     const isAvatarHere = x === avatarX && y === avatarY
     const black = false;
   
     const piece = isAvatarHere ? <Avatar /> : null
   
-    return (
-      <div onClick={() => handleSquareClick(x, y)} key={i} style={{ width: '25%', height: '10%' }} >
-        <Square black={black}>{piece}</Square>
+    if (x === 0) {
+      return (
+        
+        <div onClick={() => handleSquareClick(x, y)} key={i} style={{ width: '20%', height: '10%' }} >
+        <Square black={black}>{piece}
+          this is a question
+        </Square>
       </div>
-    )
+      )
+    } else {
+      return (
+        <div onClick={() => handleSquareClick(x, y)} key={i} style={{ width: '20%', height: '10%' }} >
+          <Square black={black}>{piece}
+          this is an answer
+          </Square>
+        </div>
+      )
+    }
+    // return (
+    //   <div onClick={() => handleSquareClick(x, y)} key={i} style={{ width: '25%', height: '10%' }} >
+    //     <Square black={black}>{piece}</Square>
+    //   </div>
+    // )
   }
 
   return (
