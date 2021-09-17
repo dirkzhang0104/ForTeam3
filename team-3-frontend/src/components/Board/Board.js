5a6e4cf3efc3a19fa0aee49386ea9f219c005514
Yang Zhang <dirkzhang0104@live.com>
2021-09-17 13:03:35 -0400

    filled in 10 dummy Qs and As

--- team-3-frontend/src/components/Board/Board.js
+++ team-3-frontend/src/components/Board/Board.js
@@ -2,7 +2,8 @@ import React from 'react'
 import Square from '../Square/Square.js'
 import Avatar from '../Avatar/Avatar.js'
 import { moveAvatar } from '../Game/Game.js'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios';
 // import { cloneDeep } from 'lodash/cloneDeep'
 
 // function renderSquare(i, [avatarX, avatarY]) {
@@ -29,15 +30,65 @@ import { useState } from 'react'
 //   // setAnswerArray(previousArray => previousArray.push([toX, toY]))
 // }
 
-export default function Board({ avatarPosition }) {
-  
-  const [answerObj, setAnswerObj] = useState({})
+// const questions = [];
+// const answers = [];
 
-  // const questionsArray = ['1. What is the action of repeating code over and over again?', '2. What is a single instruction for a computer to perform a specific task?', '3. What is a symbol that works as a placeholder for data?', '', 'q5', 'q6', 'q7', 'q8', 'q9', 'q10']
-  // const answersArray = ['a. Algorithm', 'b. Loop', 'c. Program', 'd. Sequence', 'a. Loop', 'b. Algorithm ', 'c. Command', 'd. Sequence', 'a. Loop', 'b. Sequence', 'c. Command', 'd. Variable']
+export default function Board({ avatarPosition }) {
+
+  const [answerObj, setAnswerObj] = useState({});
+  // const [questions, setQuestions] = useState([]);
+  // const [answers, setAnswers] = useState([]);
+
+
+  const questionsArray = ['1. What is the action of repeating code over and over again?',
+  '2. What is a single instruction for a computer to perform a specific task?',
+  '3. What is a symbol that works as a placeholder for data?',
+  '4. Which of the following is a primitive data type?',
+  '5. What is computer programming?',
+  '6. Which of the following is a programming language?',
+  '7. What data type is \"Hello World\"?',
+  '8. What is an algorithm?',
+  '9. What is pair programming?',
+  '10. What two numbers are used in binary?']
+  const answersArray = ['a. Algorithm', 'b. Loop', 'c. Program', 'd. Sequence', 'a. Loop', 'b. Algorithm ', 'c. Command', 'd. Sequence', 'a. Loop', 'b. Sequence', 'c. Command', 'd. Variable', 'a. Boolean', 'b. Object', 'c. String', 'd. Array', 'a. List of functions', 'b. Telling a computer what to do', 'c. TV show', 'd. Radio show', 'a. Java', 'b. Cobra', 'c. Byte', 'd. VS Code', 'a. Text', 'b. Array', 'c. Quote', 'd. String', 'a. Questions for a machine', 'b. A user name', 'c. Brains for a robot', 'd. Instructions for a computer', 'a. Programming alone', 'b. Programming with another developer', 'c. Working with two computers', 'd. Working on two projects at once', 'a. 1 and 2', 'b. 5 and 10', 'c. 0 and 1', 'd. 2 and 10']
+
+  // useEffect(() => {
+  //   getQs();
+  // }, [])
+
+  // function getQs() {
+  //   axios.get("http://localhost:8080/questions")
+  //   .then(rawData => {
+  //     // console.log(rawData.data)
+  //     let arrOfAs = [];
+  //     rawData.data.forEach(obj => {
+  //       // console.log(obj.answersList.map(obj => {
+  //       //   return obj.answer;
+  //       // }))
+  //       let innerArr = obj.answersList.map(obj => {
+  //         return obj.answer;
+  //       });
+  //       // console.log(innerArr)
+  //       innerArr.forEach(str => {
+  //         // console.log(str)
+  //         arrOfAs.push(str);
+  //         // console.log(arrOfAs)
+  //       })
+  //     })
+  //     // console.log(answers)
+  //     setAnswers(arrOfAs);
+  //     // console.log(arrOfAs)
+  //     setQuestions(rawData.data.map(obj => {
+  //       return obj.question
+  //     }));
+  //   })
+  //   .catch(err => {
+  //     console.log(err)
+  //   })
+  // }
 
   const squares = []
-  for (let i = 0; i < 40; i++) {
+  for (let i = 0; i < 50; i++) {
     squares.push(renderSquare(i, avatarPosition))
   }
 
@@ -52,8 +103,8 @@ export default function Board({ avatarPosition }) {
     } else {
       alert("please answer the previous question")
     }
-    
-    
+
+
     // console.log(answerObj)
   }
 
@@ -61,19 +112,19 @@ export default function Board({ avatarPosition }) {
 
     const x = i % 5
     const y = Math.floor(i / 5)
-  
+
     const isAvatarHere = x === avatarX && y === avatarY
     const black = false;
-  
+
     const piece = isAvatarHere ? <Avatar /> : null
-  
+
     if (x === 0) {
       return (
-        
+
       <div onClick={() => handleSquareClick(x, y)} key={i} style={{ width: '20%', height: '10%' }} >
         <Square black={black}>{piece}
-        These are questions
-          {/* {questionsArray.shift()} */}
+        {/* These are questions */}
+          {questionsArray.shift()}
         </Square>
       </div>
       )
@@ -81,8 +132,9 @@ export default function Board({ avatarPosition }) {
       return (
         <div onClick={() => handleSquareClick(x, y)} key={i} style={{ width: '20%', height: '10%' }} >
           <Square black={black}>{piece}
-          These are answers
-            {/* {answersArray.shift()} */}
+          {/* These are answers */}
+          {/* {console.log(answers)} */}
+            {answersArray.shift()}
           </Square>
         </div>
       )
@@ -106,8 +158,4 @@ export default function Board({ avatarPosition }) {
       {squares}
     </div>
   )
-}
-
-function getQuestions() {
-  
 }
